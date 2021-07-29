417054472c90f565ddad1ccf36e68dc36f8cc723
mbaire <mbairemunge@gmail.com>
2021-07-29 14:16:00 +0300

    Modified Js file

--- JS/index.js
+++ JS/index.js
@@ -19,7 +19,7 @@ var akanName = function(){
       ];
       var femaleNames = [
         "Akosua",
-         " Adwoa",
+        "Adwoa",
         "Abenaa",
         "Akua",
         "Yaa",
@@ -27,7 +27,15 @@ var akanName = function(){
         "Ama"
       ];
 
-      var name
+      var name= (document .getElementById("NAME").value);
+      var day= parseInt(document .getElementById("DD").value);
+      var month= parseInt(document .getElementById("MM").value);
+      var year= parseInt(document .getElementById("YY").value);
+      var maleNames=(document .getElementById("maleGender"));
+      var femaleNames=(document .getElementById("femaleGender"));
+      var output=(document .getElementById("output"));
+
+
 
 
 
